a7477a9b51579b82dc731e9d064391bfe59a8c47
yxb123456 <2186256471@qq.com>
2025-08-31 14:52:54 +0800

    feat(base): 引入Rsdoctor

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginVue } from '@rsbuild/plugin-vue';
-
+import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
 export default defineConfig({
   plugins: [pluginVue()],
+  performance: {
+    buildCache: false,
+  },
+  tools: {
+    bundlerChain: (chain, { CHAIN_ID }) => {
+      chain.plugin('Rsdoctor').use(RsdoctorRspackPlugin, [
+        {
+          // 插件选项
+        },
+      ]);
+    },
+  },
 });
