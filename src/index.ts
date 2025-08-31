38408256754ce015370e3d52f2ac41e641ef74be
yxb123456 <2186256471@qq.com>
2025-08-31 15:24:52 +0800

    feat(arco): 全量导入ArcoDesignVue组件库

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
+// 引入ArcoVue ICON库
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import pinia from './store/base';
 
 const app = createApp(App)
+// 使用ArcoVue 全量注册;
+app.use(ArcoVue);
+// 使用 ArcoVue ICON库
+app.use(ArcoVueIcon);
 // 挂载pinia
 app.use(pinia);
 app.mount('#root');
